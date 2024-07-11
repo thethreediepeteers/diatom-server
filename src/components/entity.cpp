3611bb03fbb80e196ebb274fea297b0cd2c56fb8
Hussain Abidi <hussainabidi2009@gmail.com>
2024-07-11 15:06:52 +0500

    revert oops

--- src/components/entity.cpp
+++ src/components/entity.cpp
@@ -7,7 +7,4 @@ Entity::Entity(int x, int y) : id(counter++), pos(XY(x, y)), vel(XY(0, 0)) {
   instances[id] = this;
 }
 
-Entity::~Entity() {
-  instances.erase(id);
-  delete this;
-}
+Entity::~Entity() { instances.erase(id); }
