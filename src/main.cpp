7e44c470cca17ec83417dad3767edd5945874603
Shitposter <iudhduhsuiskjdakjncjsdou@national.shitposting.agency>
2025-01-01 17:03:09 +0500

    [Feat] Tried to add diep io stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 #include "modules/config.h"
 #include "modules/physics.h"
 #include "modules/server.h"
+#include "modules/util.h"
+#include "components/controllers.h"
+
 #include <csignal>
 #include <iostream>
+#include <random>
+#include <ctime>
 
 void setupSignals();
 void tick();
@@ -60,4 +65,25 @@ us_timer_t* setupLoop() {
       1000 / config::SERVER_FPS);
 
   return delayTimer;
+}
+
+void executeSpawner() {
+  //config::MAP_WIDTH
+  srand(time(NULL));
+
+  auto entityX = rand() / RAND_MAX * config::MAP_WIDTH;
+  auto entityY = rand() / RAND_MAX * config::MAP_HEIGHT;
+
+  Entity* entity = new Entity(entityX, entityY, 0, 3, *(new util::HexColor(0, 0, 0)), 0);
+
+  entity->spawn("");
+}
+
+void setupSpawner() {
+  uWS::Loop* loop = uWS::Loop::get();
+  us_timer_t* delayTimer = us_create_timer((us_loop_t*)loop, 0, 0);
+
+  us_timer_set(delayTimer, [](us_timer_t*) {
+    executeSpawner();
+  }, 5000, 5000);
 }
--- src/modules/config.h
+++ src/modules/config.h
@@ -5,8 +5,10 @@ namespace config {
   const int SERVER_FPS = 20;
   const int IP_LIMIT = 0;
 
-  const int MAP_WIDTH = 1024;
-  const int MAP_HEIGHT = 1024;
+  const int MAP_WIDTH = 2048;
+  const int MAP_HEIGHT = 2048;
 
   const int ROOM_BOUNCE = 3;
+
+  const int BALLS_COUNT = 50;
 } // namespace config
